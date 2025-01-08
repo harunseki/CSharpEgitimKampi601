dbefd5c01beca6b73fab94fb63a72c69b31a7a64
harunseki <harunseki@gmail.com>
2025-01-08 08:30:29 +0300

    Add project files.

---
+++ CSharpEgitimKampi601/Form1.cs
@@ -0,0 +1,39 @@
+﻿using CSharpEgitimKampi601.Entities;
+using CSharpEgitimKampi601.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi601
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        CustomerOperations customerOperations = new CustomerOperations();
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var customer = new Customer()
+            {
+                CustomerName = textBox2.Text,
+                CustomerSurname = textBox3.Text,
+                CustomerCity = textBox4.Text,
+                CustomerBalance = decimal.Parse(textBox5.Text),
+                CustomerShoppingCount = int.Parse(textBox6.Text),
+            };
+
+            customerOperations.AddCustomer(customer);
+            MessageBox.Show("Ekleme başarılı...","Uyarı");
+        }
+    }
+}
---
+++ CSharpEgitimKampi601/Services/MondoDbConnection.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi601.Services
+{
+    public class MondoDbConnection
+    {
+        private IMongoDatabase _database;
+
+        public MondoDbConnection()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            _database = client.GetDatabase("CSharpEgitimKampi601Db");
+        }
+
+        public IMongoCollection<BsonDocument> GetCustomersCollection()
+        {
+            return _database.GetCollection<BsonDocument>("Customers");
+        }
+    }
+}
